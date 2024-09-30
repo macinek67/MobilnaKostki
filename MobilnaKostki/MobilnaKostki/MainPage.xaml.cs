b92a5b2b54c4461c52d126ff52a32e7be7ae3855
marcin.gawron <marcindzik1234@gmail.com>
2024-09-30 12:44:22 +0200

    ukonczono back-end aplikacji, ukonczenie aplikacji

--- MobilnaKostki/MobilnaKostki/MainPage.xaml.cs
+++ MobilnaKostki/MobilnaKostki/MainPage.xaml.cs
@@ -10,19 +10,75 @@ namespace MobilnaKostki
 {
     public partial class MainPage : ContentPage
     {
+        public static List<Image> listaKosci;
+        public static List<int> wylosowaneLiczby = new List<int>();
+        public static int liczbaPunktowJedenLos = 0;
+        public static int liczbaPunktowWszystkie = 0;
         public MainPage()
         {
             InitializeComponent();
+            listaKosci = new List<Image>()
+            {
+                kostka1, kostka2, kostka3, kostka4, kostka5
+            };
+        }
+        public void losujLiczbyDoKostek()
+        {
+            Random random = new Random();
+            for (int i = 1; i <= 5; i++)
+            {
+                int wylosowanaLiczba = random.Next(1, 7);
+                wylosowaneLiczby.Add(wylosowanaLiczba);
+                listaKosci[i-1].Source = "liczba" + wylosowanaLiczba + ".png";
+            }
+            liczbaPunktowJedenLos = liczPunkty();
+            wynikLoss.Text = "Wynik tego losowania: " + liczbaPunktowJedenLos;
+            wynikGry.Text = "Wynik gry: " + liczbaPunktowWszystkie;
+        }
+
+        public int liczPunkty()
+        {
+            for (int j = 1; j <= 6; j++)
+            {
+                int liczPowtorki = 0;
+                int wynik = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (wylosowaneLiczby[i] == j)
+                    {
+                        liczPowtorki++;
+                        wynik += j;
+                    }
+                }
+
+                if (liczPowtorki >= 2)
+                {
+                    liczbaPunktowJedenLos += wynik;
+                    liczbaPunktowWszystkie += wynik;
+                }
+            }
+
+            return liczbaPunktowJedenLos;
         }
 
         private void rzutKosciButton_Clicked(object sender, EventArgs e)
         {
-
+            liczbaPunktowJedenLos = 0;
+            wylosowaneLiczby.Clear();
+            losujLiczbyDoKostek();
         }
 
         private void resetujWynikButton_Clicked(object sender, EventArgs e)
         {
-
+            liczbaPunktowWszystkie = 0;
+            liczbaPunktowJedenLos = 0;
+            wynikLoss.Text = "Wynik tego losowania: " + liczbaPunktowJedenLos;
+            wynikGry.Text = "Wynik gry: " + liczbaPunktowWszystkie;
+            for (int i = 1; i <= 5; i++)
+            {
+                listaKosci[i - 1].Source = "pytajnik.png";
+            }
         }
     }
 }
